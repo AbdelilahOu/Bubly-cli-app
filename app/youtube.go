7c6c1af3102cea98ae8003842a45386ecdb7d55c
AbdelilahOu <abdelilah4dev@gmail.com>
2024-02-08 09:28:03 +0000

    FEAT: update displayed text

--- app/youtube.go
+++ app/youtube.go
@@ -6,7 +6,21 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// The second view, after a task has been Youtube
+var YoutubeOptions = []ViewsOptions{
+	{
+		View:        "translate",
+		ChoiceLabel: "Youtube vedio translator",
+	},
+	{
+		View:        "download",
+		ChoiceLabel: "Youtube vedio downloader",
+	},
+	{
+		View:        "infos",
+		ChoiceLabel: "Youtube vedio infos",
+	},
+}
+
 func YoutubeView(m AppModel) string {
 	c := m.Choice
 
@@ -14,22 +28,20 @@ func YoutubeView(m AppModel) string {
 
 	choices := fmt.Sprintf(
 		"%s\n%s\n%s\n",
-		checkbox("Youtube vedio translator", c == 0),
-		checkbox("Youtube vedio downloader", c == 1),
-		checkbox("Youtube vedio infos", c == 2),
+		checkbox(YoutubeOptions[0].ChoiceLabel, c == 0),
+		checkbox(YoutubeOptions[1].ChoiceLabel, c == 1),
+		checkbox(YoutubeOptions[2].ChoiceLabel, c == 2),
 	)
 
 	return fmt.Sprintf(tpl, choices)
 }
 
-// Update loop for the second view after a choice has been made
 func UpdateYoutube(msg tea.Msg, m AppModel) (tea.Model, tea.Cmd) {
-	YoutubeChoices := []string{"carrot planting", "market trip", "reading time"}
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "j", "down":
-			if len(YoutubeChoices) > m.Choice+1 {
+			if len(YoutubeOptions) > m.Choice+1 {
 				m.Choice++
 			}
 		case "k", "up":
